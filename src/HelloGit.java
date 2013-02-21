58c06bee5ffd640bbf5b47918ce4ef9b34b5e808
Jason Steinberg <jsteinbe@mines.edu>
2013-02-21 16:08:53 -0700

    Added src back in

---
+++ src/HelloGit.java
@@ -0,0 +1,14 @@
+
+public class HelloGit {
+
+	public static void crazyIdea(int i) {
+		System.out.println("Your crazy idea cost me "+ i*100 + " yen!");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		crazyIdea(45);
+
+	}
+
+}
