65bb7e48b6f86c7d62e42771e7906e89d4e765e7
Austin Diviness <adivines@mines.edu>
2013-02-21 15:45:35 -0700

    bender1 and benderA added

--- robot.py
+++ robot.py
@@ -5,4 +5,16 @@ class Robot:
 
     def talk(self):
         print "Bite my shiny metal ass"
+
+
+class BenderA(Robot):
+    def smoke(self):
+        print "puff puff"
+
+class Bender1(Robot):
+
+    def talk(self):
+        print "Bite my glorious metal ass"
+
+
         
